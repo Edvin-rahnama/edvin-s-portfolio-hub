50370c247c54508c1fead698c4482f8db70ddc0e
e.rahnama <e.rahnama@t2consult.net>
2025-12-17 23:09:23 +0100

    Revert "fix: configure app for github pages subpath"
    This reverts commit 69fff38831430e9604b81d2e746d9d5c297f6ce2.

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter basename="/edvin-s-portfolio-hub">
+          <BrowserRouter>
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="*" element={<NotFound />} />
